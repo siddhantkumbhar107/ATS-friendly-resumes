d9a1c87f058dac4cc08a439239f7843eff848bb2
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-04-16 12:22:40 +0530

    Add PDF export functionality using xhtml2pdf

---
+++ pdf_export.py
@@ -0,0 +1,10 @@
+from xhtml2pdf import pisa
+from io import BytesIO
+
+
+def convert_html_to_pdf(source_html):
+    output = BytesIO()
+    pisa_status = pisa.CreatePDF(source_html, dest=output)
+    if pisa_status.err:
+        return None
+    return output.getvalue()
