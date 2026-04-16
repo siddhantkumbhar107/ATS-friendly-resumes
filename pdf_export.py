8a7ea5f2a34b3449a24ddcf12715106a97c5e747
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-04-16 13:33:15 +0530

    Update pdf_export.py

--- pdf_export.py
+++ pdf_export.py
@@ -1,9 +1,14 @@
-from xhtml2pdf import pisa
-from io import BytesIO
+try:
+    from xhtml2pdf import pisa
+    from io import BytesIO
 
-def convert_html_to_pdf(source_html):
-    output = BytesIO()
-    pisa_status = pisa.CreatePDF(source_html, dest=output)
-    if pisa_status.err:
+    def convert_html_to_pdf(source_html):
+        output = BytesIO()
+        pisa_status = pisa.CreatePDF(source_html, dest=output)
+        if pisa_status.err:
+            return None
+        return output.getvalue()
+
+except ImportError:
+    def convert_html_to_pdf(source_html):
         return None
-    return output.getvalue()
